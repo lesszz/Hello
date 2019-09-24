6384fd3ff37fa131e2006199c8b2440b64ea90e9
lesszz <1062726126@qq.com>
2019-09-24 21:59:28 +0800

    say HelloMorning,World

---
+++ GoodMorning.java
@@ -0,0 +1,11 @@
+//  GoodMorning.java
+class GoodMorning {
+	public static void main(String[] args) {
+		System.out.print("Hello");
+		sayMorning();
+		System.out.println("World");
+	}
+	public static void sayMorning() {
+		System.out.print("Morning,");
+	}
+}
