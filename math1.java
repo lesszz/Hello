966a87dc651b2019f2c8c7435ddb42a8431f6e17
lesszz <1062726126@qq.com>
2019-09-29 19:37:23 +0800

    math

---
+++ math1.java
@@ -0,0 +1,9 @@
+public class math1 {
+	public static void main(String[] args) {
+		int count = 10;
+		count *= count + 2;
+		count /= 100;
+		count++;
+	    System.out.println("count:"+count);
+	}
+}
